1e14e1656e2db0f268a70da9fac32678b80c618e
Diego B Theuerkauf <diego.theuerkauf@tuebingen.mpg.de>
2024-10-24 22:05:20 +0200

    Change theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -75,7 +75,7 @@ execution_timeout = 1000  # This is the timeout for executing a notebook cell
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "piccolo_theme"  # 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"  # 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
